850c2552025dc4e4f7a8ab83ec9f0d40e40e9771
Jonas <142022025+Jonserafim7@users.noreply.github.com>
2024-09-06 17:25:36 -0300

    feat: instalação do shadcnui e dependencias

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+import tailwindcssAnimate from 'tailwindcss-animate'
+
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ['class'],
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        // Your existing theme configuration
         screens: {
             xs: '320px',
             sm: '480px',
@@ -11,7 +15,6 @@ export default {
             '2xl': '1440px',
             '3xl': '1920px',
         },
-
         container: {
             center: true,
             screens: {
@@ -22,18 +25,16 @@ export default {
                 '2xl': '1440px',
             },
         },
-
         maxWidth: {
-            xxs: '20rem', // 320px
-            xs: '25rem', // 400px
-            sm: '30rem', // 480px
-            md: '35rem', // 560px
-            lg: '48rem', // 768px
-            xl: '64rem', // 1024px
-            xxl: '80rem', // 1280px
+            xxs: '20rem',
+            xs: '25rem',
+            sm: '30rem',
+            md: '35rem',
+            lg: '48rem',
+            xl: '64rem',
+            xxl: '80rem',
             full: '100%',
         },
-
         boxShadow: {
             xxsmall: '0px 1px 2px rgba(0, 0, 0, 0.05)',
             xsmall: '0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)',
@@ -43,58 +44,56 @@ export default {
             xlarge: '0px 24px 48px -12px rgba(0, 0, 0, 0.18)',
             xxlarge: '0px 32px 64px -12px rgba(0, 0, 0, 0.14)',
         },
-
         fontSize: {
-            xs: ['0.75rem', { lineHeight: '1.5' }], // 12px
-            sm: ['0.875rem', { lineHeight: '1.5' }], // 14px
-            base: ['1rem', { lineHeight: '1.5' }], // 16px
-            md: ['1.125rem', { lineHeight: '1.5' }], // 18px
-            lg: ['1.25rem', { lineHeight: '1.5' }], // 20px
-            xl: ['1.25rem', { lineHeight: '1.4' }], // 20px
-            '2xl': ['1.5rem', { lineHeight: '1.4' }], // 24px
-            '3xl': ['1.75rem', { lineHeight: '1.4' }], // 28px
-            '4xl': ['2rem', { lineHeight: '1.3' }], // 32px
-            '5xl': ['2.25rem', { lineHeight: '1.2' }], // 36px
-            '6xl': ['2.5rem', { lineHeight: '1.2' }], // 40px
-            '7xl': ['2.75rem', { lineHeight: '1.2' }], // 40px
-            '8xl': ['3rem', { lineHeight: '1.2' }], // 48px
-            '9xl': ['3.25rem', { lineHeight: '1.2' }], // 52px
-            '10xl': ['3.5rem', { lineHeight: '1.2' }], // 56px
+            xs: ['0.75rem', { lineHeight: '1.5' }],
+            sm: ['0.875rem', { lineHeight: '1.5' }],
+            base: ['1rem', { lineHeight: '1.5' }],
+            md: ['1.125rem', { lineHeight: '1.5' }],
+            lg: ['1.25rem', { lineHeight: '1.5' }],
+            xl: ['1.25rem', { lineHeight: '1.4' }],
+            '2xl': ['1.5rem', { lineHeight: '1.4' }],
+            '3xl': ['1.75rem', { lineHeight: '1.4' }],
+            '4xl': ['2rem', { lineHeight: '1.3' }],
+            '5xl': ['2.25rem', { lineHeight: '1.2' }],
+            '6xl': ['2.5rem', { lineHeight: '1.2' }],
+            '7xl': ['2.75rem', { lineHeight: '1.2' }],
+            '8xl': ['3rem', { lineHeight: '1.2' }],
+            '9xl': ['3.25rem', { lineHeight: '1.2' }],
+            '10xl': ['3.5rem', { lineHeight: '1.2' }],
         },
         extend: {
             spacing: {
-                px: '1px', // 1px
-                0: '0px', // 0px
-                1: '0.25rem', // 4px
-                2: '0.5rem', // 8px
-                3: '0.75rem', // 12px
-                4: '1rem', // 16px
-                5: '1.25rem', // 20px
-                6: '1.5rem', // 24px
-                8: '2rem', // 32px
-                10: '2.5rem', // 40px
-                12: '3rem', // 48px
-                14: '3.5rem', // 56px
-                16: '4rem', // 64px
-                18: '4.5rem', // 72px
-                20: '5rem', // 80px
-                24: '6rem', // 96px
-                28: '7rem', // 112px
-                30: '7.5rem', // 120px
-                32: '8rem', // 128px
-                36: '9rem', // 144px
-                40: '10rem', // 160px
-                44: '11rem', // 176px
-                48: '12rem', // 192px
-                52: '13rem', // 208px
-                56: '14rem', // 224px
-                60: '15rem', // 240px
-                64: '16rem', // 256px
-                72: '18rem', // 288px
-                80: '20rem', // 320px
-                96: '24rem', // 384px
+                0: '0px',
+                1: '0.25rem',
+                2: '0.5rem',
+                3: '0.75rem',
+                4: '1rem',
+                5: '1.25rem',
+                6: '1.5rem',
+                8: '2rem',
+                10: '2.5rem',
+                12: '3rem',
+                14: '3.5rem',
+                16: '4rem',
+                18: '4.5rem',
+                20: '5rem',
+                24: '6rem',
+                28: '7rem',
+                30: '7.5rem',
+                32: '8rem',
+                36: '9rem',
+                40: '10rem',
+                44: '11rem',
+                48: '12rem',
+                52: '13rem',
+                56: '14rem',
+                60: '15rem',
+                64: '16rem',
+                72: '18rem',
+                80: '20rem',
+                96: '24rem',
+                px: '1px',
             },
-
             colors: {
                 brand: {
                     black: '#000000',
@@ -119,8 +118,8 @@ export default {
                     'error-red-light': '#fef3f2',
                 },
                 background: {
-                    DEFAULT: '#ffffff', // bg-background, text-background, border-background,
-                    primary: '#ffffff', // bg-background-primary, text-background-primary, border-background-primary,
+                    DEFAULT: '#ffffff',
+                    primary: '#ffffff',
                     secondary: '#eeeeee',
                     tertiary: '#666666',
                     alternative: '#000000',
@@ -151,7 +150,34 @@ export default {
                     alternative: '#ffffff',
                 },
             },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: {
+                        height: '0',
+                    },
+                    to: {
+                        height: 'var(--radix-accordion-content-height)',
+                    },
+                },
+                'accordion-up': {
+                    from: {
+                        height: 'var(--radix-accordion-content-height)',
+                    },
+                    to: {
+                        height: '0',
+                    },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+            },
         },
     },
-    plugins: [],
+    plugins: [tailwindcssAnimate],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
         port: 3000, // Make sure the port matches your Docker configuration.
         watch: {
             usePolling: true, // Necessary for some environments like Docker where file changes might not be detected.
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
         },
     },
 })
