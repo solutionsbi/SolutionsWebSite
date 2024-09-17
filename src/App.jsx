9a1072ac98b8de3512a34ca4b08b53a12ce81d0a
Jonas <142022025+Jonserafim7@users.noreply.github.com>
2024-09-17 18:03:31 -0300

    feat: finalizada a animação do mapa mundi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ReactLenis, useLenis } from 'lenis/react'
+import { ReactLenis } from 'lenis/react'
 
 import Layout from './Pages/Layout/Layout'
 import Home from './Pages/Home'
