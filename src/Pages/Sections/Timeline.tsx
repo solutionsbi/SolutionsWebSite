90b1bf1f01ed100e03bda1cfed803447f20845a3
Jonas <142022025+Jonserafim7@users.noreply.github.com>
2024-09-18 23:02:55 -0300

    fix: correção de texto no componente timeline da página sobre

--- src/Pages/Sections/Timeline.tsx
+++ src/Pages/Sections/Timeline.tsx
@@ -1,21 +1,20 @@
-type badge = {
-    icon: string
-    stat: string
-    value: string
-}
+import { UserPlus } from 'lucide-react'
+import { Database } from 'lucide-react'
 
 type TimeLineCardProps = {
     date: string
     title: string
     description: string
-    badges: badge[]
+    clients: number
+    projects: number
 }
 
 export const TimeLineCard: React.FC<TimeLineCardProps> = ({
     date,
     title,
     description,
-    badges,
+    clients,
+    projects,
 }) => {
     return (
         <div className="relative flex flex-col border-b border-white/10 pb-10">
@@ -36,28 +35,18 @@ export const TimeLineCard: React.FC<TimeLineCardProps> = ({
             <h4 className="mb-4 font-inter text-4xl font-semibold normal-case">
                 {title}
             </h4>
-            <p className="mb-8 drop-shadow-custom md:text-justify">
-                {description}
-            </p>
+            <p className="mb-8 drop-shadow-custom">{description}</p>
             <div className="flex gap-2">
-                {badges.map((badge, index) => (
-                    <div
-                        key={index}
-                        className="shadow-medium relative flex items-center gap-2 rounded-[5px] border border-white/10 px-4 py-3"
-                    >
-                        <img
-                            className="h-auto w-4"
-                            src={badge.icon}
-                            alt={'stat icon'}
-                        />
-                        <span className="text-sm lg:text-base">
-                            {badge.stat}:
-                        </span>
-                        <span className="text-sm lg:text-base">
-                            {badge.value}
-                        </span>
-                    </div>
-                ))}
+                <div className="shadow-medium relative flex w-max items-center gap-3 rounded-[5px] border border-white/10 px-4 py-3">
+                    <UserPlus className="h-auto w-6" />
+                    <span className="text-sm lg:text-base">Clientes:</span>
+                    <span className="text-sm lg:text-base">{clients}</span>
+                </div>
+                <div className="shadow-medium relative flex w-max items-center gap-3 rounded-[5px] border border-white/10 px-4 py-3">
+                    <Database className="h-auto w-6" />
+                    <span className="text-sm lg:text-base">Projetos:</span>
+                    <span className="text-sm lg:text-base">{projects}</span>
+                </div>
             </div>
         </div>
     )
@@ -68,109 +57,49 @@ const timeLineData: TimeLineCardProps[] = [
         date: '2019',
         title: 'Do Início à Escalabilidade',
         description:
-            'Tudo começou em 2019 com a impressão de alguns cartões de apresentação e o primeiro projeto com um cliente do RJ chamado S3Security. Esse mesmo voltaria a ser nosso cliente em 2023​.Nosso primeiro parceiro Julio Leite (Insight) se juntou ao time trazendo outros projetos para darmos continuidade no crescimento​.',
-        badges: [
-            {
-                icon: '/src/assets/svg/database-icon.svg',
-                stat: 'Clientes',
-                value: '17',
-            },
-            {
-                icon: '/src/assets/svg/person-icon.svg',
-                stat: 'Projetos',
-                value: '5',
-            },
-        ],
+            'Tudo começou em 2019 com a impressão de alguns cartões de apresentação e o primeiro projeto com um cliente do RJ. Esse mesmo voltaria a ser nosso cliente em 2023. Nosso primeiro parceiro se juntou ao time trazendo outros projetos para darmos continuidade no crescimento.',
+        clients: 17,
+        projects: 5,
     },
     {
         date: '2020',
         title: 'Da Diversificação à uma Nova Empresa',
         description:
-            'Nós diversificamos nossos serviços atuando em setores como TI, Medicina, Industrias e Logística​.A SolutionsBI tem sua firma aberta perante a lei, consolida seus clientes e contrata seu primeiro colaborador​',
-        badges: [
-            {
-                icon: '/src/assets/svg/database-icon.svg',
-                stat: 'Clientes',
-                value: '17',
-            },
-            {
-                icon: '/src/assets/svg/person-icon.svg',
-                stat: 'Projetos',
-                value: '5',
-            },
-        ],
+            'Nós diversificamos nossos serviços atuando em setores como TI, Medicina, Industrias e Logística. A SolutionsBI tem sua firma aberta perante a lei, consolida seus clientes e contrata seu primeiro colaborador.',
+        clients: 15,
+        projects: 5,
     },
     {
         date: '2021',
         title: 'Globalização',
         description:
-            'Estamos agora presentes em projetos em 4 continentes diferentes aumentando nosso time e parcerias .​',
-        badges: [
-            {
-                icon: '/src/assets/svg/database-icon.svg',
-                stat: 'Clientes',
-                value: '28',
-            },
-            {
-                icon: '/src/assets/svg/person-icon.svg',
-                stat: 'Projetos',
-                value: '8',
-            },
-        ],
+            'Estamos agora presentes em projetos em 4 continentes diferentes aumentando nosso time e parcerias.',
+        clients: 28,
+        projects: 8,
     },
     {
         date: '2022',
         title: 'Crescimento',
         description:
-            'A SolutionsBI está agora presente em grandes projetos Nacionais e Internacionais. Temos um time consolidado e engajado . Nesse ponto ja atuamos em projetos no Brasil, Inglaterra, Alemanha, Portugal, USA, Uruguai e Barbados​.​',
-        badges: [
-            {
-                icon: '/src/assets/svg/database-icon.svg',
-                stat: 'Clientes',
-                value: '50',
-            },
-            {
-                icon: '/src/assets/svg/person-icon.svg',
-                stat: 'Projetos',
-                value: '12',
-            },
-        ],
+            'A SolutionsBI está agora presente em grandes projetos Nacionais e Internacionais. Temos um time consolidado e engajado. Nesse ponto já atuamos em projetos no Brasil, Inglaterra, Alemanha, Portugal, USA, Uruguai e Barbados.',
+        clients: 50,
+        projects: 12,
     },
     {
         date: '2023',
         title: 'Amadurecimento',
         description:
-            'Nessa fase criamos processos internos para gestão financeira, contratação, pessoas, crescimento profissional e marketing. Participamos ativamente de projetos da cidade e fechamos uma parceria internacional exclusiva​.​',
-        badges: [
-            {
-                icon: '/src/assets/svg/database-icon.svg',
-                stat: 'Clientes',
-                value: '64',
-            },
-            {
-                icon: '/src/assets/svg/person-icon.svg',
-                stat: 'Projetos',
-                value: '12',
-            },
-        ],
+            'Nessa fase criamos processos internos para gestão financeira, contratação, pessoas, crescimento profissional e marketing. Participamos ativamente de projetos da cidade e fechamos uma parceria internacional exclusiva.',
+        clients: 64,
+        projects: 12,
     },
     {
         date: '2024',
         title: 'Tração',
         description:
-            'Buscando um crescimento exponencial, diversificamos nossos serviços, criamos nossos produtos e investimos em recursos para aumentar ainda mais a qualidade de nosso atendimento perante nossos cliente​',
-        badges: [
-            {
-                icon: '/src/assets/svg/database-icon.svg',
-                stat: 'Clientes',
-                value: '150',
-            },
-            {
-                icon: '/src/assets/svg/person-icon.svg',
-                stat: 'Projetos',
-                value: '12',
-            },
-        ],
+            'Buscando um crescimento exponencial, diversificamos nossos serviços, criamos nossos produtos e investimos em recursos para aumentar ainda mais a qualidade de nosso atendimento perante nossos clientes.',
+        clients: 150,
+        projects: 12,
     },
 ]
 
@@ -187,7 +116,7 @@ export default function Timeline() {
                             </span>{' '}
                             ao <br /> Impacto Global
                         </h2>
-                        <p className="leading-normal drop-shadow-custom md:text-justify xl:text-md">
+                        <p className="leading-normal drop-shadow-custom xl:text-md">
                             Fundada em 2019, a Solutions BI nasceu com a missão
                             de transformar dados em insights valiosos para
                             empresas de diversos setores. Desde o início, nosso
@@ -207,7 +136,8 @@ export default function Timeline() {
                                 date={timeLine.date}
                                 title={timeLine.title}
                                 description={timeLine.description}
-                                badges={timeLine.badges}
+                                clients={timeLine.clients}
+                                projects={timeLine.projects}
                             />
                         ))}
                     </div>
